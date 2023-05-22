8ec3d8388f92bc32efdc58a8fe90fd27b6d23d99
szymqn <koodziejskisz@outlook.com>
2023-05-22 20:45:58 +0200

    add delete mode for user

--- user/urls.py
+++ user/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('account/', login_required(views.account_details), name='account'),
+
+    path('account/delete/<int:pk>', login_required(views.AccountDelete.as_view()), name='deleteAccount'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.views.generic import FormView, CreateView
+from django.views.generic.edit import FormView, CreateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import CustomUser
@@ -22,3 +22,12 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "user/signup.html"
+
+
+class AccountDelete(DeleteView):
+    model = CustomUser
+    context_object_name = 'CustomUser'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        return super(AccountDelete, self).form_valid(form)
